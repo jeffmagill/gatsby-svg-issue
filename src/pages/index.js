92a31af7e93596e1a08ddec0db41607c20953e23
Jeff Magill <jeff@unhingedweb.com>
2019-04-03 22:59:40 -0400

    copying package.json and gatsby-config.js from https://github.com/jacobmischka/gatsby-plugin-react-svg-gatsby-new-example

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,10 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 import ResearchIcon from '../images/research.svg'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
